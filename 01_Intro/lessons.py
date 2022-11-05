bd61fb4c067d580198cd460f97e948acb610548e
Ryan Charette <100053398+ryan-charette@users.noreply.github.com>
2022-11-05 13:44:04 -0500

    Update lessons.py

--- 01_Intro/lessons.py
+++ 01_Intro/lessons.py
@@ -68,3 +68,14 @@ from simulate import simulate
 num_people_in_room = 2 #Change This Number (keep it smaller than 100 to save processing power)
 
 simulate(num_people_in_room) 
+
+# Lesson 7
+# Change these numbers and run to predict species!
+
+mystery_penguin_flipper = 180
+mystery_penguin_bill = 35
+
+# The model will use those values and the dataset to predict a species
+from code import predict
+predict(mystery_penguin_flipper,mystery_penguin_bill)
+
